dd7e37c09f2fdd2c9886307b3e5b8c7b20b50e21
Jabreezy7 <mohammedbaled7@gmail.com>
2024-10-25 23:05:49 -0400

    Adjust Image Sizes

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import './Hero.css'
 
 // import hero_image from '../Assets/images/Photoshoot-Images/muth-edit-front-nobg.png'
-import image_1 from '../Assets/images/Photoshoot-Images/1-MBF-nobg.png'
-import image_2 from '../Assets/images/Photoshoot-Images/2-MBF.jpg'
-import image_3 from '../Assets/images/Photoshoot-Images/3-MBF.jpg'
+import image_1 from '../Assets/images/Photoshoot-Images/1-MBF-nobg-small.png'
+import image_2 from '../Assets/images/Photoshoot-Images/2-MBF-small.jpg'
+import image_3 from '../Assets/images/Photoshoot-Images/3-MBF-small.jpg'
 
 export const Hero = () => {
     return (
 
         <div className="shopping-area">
-            {/* <div className="product">
-                <img src={image_1} alt="Product 1" />
+            <div className="product">
+                <img src={image_1} loading='lazy' alt="Product 1" />
                 <a href="" className='overlay-text'>SHOP ALL</a>
             </div>
             <div className="product">
-                <img src={image_2} alt="Product 2" />
+                <img src={image_2} loading='lazy' alt="Product 2" />
                 <a href="" className='overlay-text'>SHOP NATIONAL TEAMS</a>
-            </div> */}
+            </div>
             <div className="product">
-                <img src={image_3} alt="Product 3" />
+                <img src={image_3} loading='lazy' alt='Product 3' />
                 <a href="" className='overlay-text'>SHOP BEST SELLERS</a>
             </div>
         </div>
