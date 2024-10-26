a8e01366eb3a87a88ed19269c21fd06a6173596e
Jabreezy7 <mohammedbaled7@gmail.com>
2024-10-26 12:06:35 -0400

    Add Popular Products

---
+++ frontend/src/Components/Assets/images/data.js
@@ -0,0 +1,43 @@
+import ronaldinho_front_img from "./Select-Player-Designs/DINHO design/Edited-Images/Ronaldinho_front_mockup-small.png";
+import kaka_front_img from "./Select-Player-Designs/Kaka design/Edited-Images/Milan_front_mockup-small.png";
+import messi_front_img from "./Select-Player-Designs/messi design/Edited-Images/messi_front_tee_3_mp-small.png";
+import ronaldo_front_img from "./Select-Player-Designs/R9 Design/Edited-Images/R9_design_(FRONT)-small.png";
+
+
+let data_product = [
+    {
+        id: 1,
+        name: "Ronaldinho Graphic Tee",
+        category: "women",
+        image: ronaldinho_front_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 2,
+        name: "Kaka Graphic Tee",
+        category: "men",
+        image: kaka_front_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    {
+        id: 3,
+        name: "Messi Graphic Tee",
+        category: "kids",
+        image: messi_front_img,
+        new_price: 50.0,
+        old_price: 80.5,
+    },
+    // {
+    //     id: 4,
+    //     name: "Ronaldo Graphic Tee",
+    //     category: "men",
+    //     image: ronaldo_front_img,
+    //     new_price: 50.0,
+    //     old_price: 80.5,
+    // },
+
+];
+
+export default data_product
