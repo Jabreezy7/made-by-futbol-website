154433c8e42cec7ae7bd6e6a02306e8c41d72d80
Jabreezy7 <mohammedbaled7@gmail.com>
2024-10-31 10:29:46 -0400

    Add New Drops Component

---
+++ frontend/src/Components/NewDrops/NewDrops.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewDrops.css'
+
+export const NewDrops = () => {
+    return (
+        <div className="new-drops">
+            <h1>NEW DROPS</h1>
+        </div>
+    )
+}
+
+export default NewDrops
